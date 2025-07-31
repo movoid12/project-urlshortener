61de425986c79cbc0a22ee5cdbf01a1dc0a252ed
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-07-31 22:03:47 +0200

    feat: add URL Shortener Microservice

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,55 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+const urlDatabase = {};
+let urlCounter = 1;
+
+function isValidUrl(userInput) {
+  return Promise.resolve(userInput)
+    .then((input) => new URL(input))
+    .then((url) => url.protocol === "http:" || url.protocol === "https:")
+    .catch(() => false);
+}
+
+app.use(express.urlencoded());
+
+// Post endpoint to create short URL
+app.post("/api/shorturl", (req, res) => {
+  const original_url = req.body.url;
+  
+  if (!isValidUrl(original_url)) {
+    return res.json({ error: "invalid url" });
+  }
+
+  const short_url = urlCounter++;
+  urlDatabase[short_url] = original_url;
+  res.json({ original_url, short_url });
 });
 
-app.listen(port, function() {
+// GET endpoint to redirect
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const short_url = req.params.short_url;
+
+  const original_url = urlDatabase[short_url];
+
+  if (original_url) {
+    return res.redirect(original_url);
+  } else {
+    return res.json({ error: "invalid url" });
+  }
+});
+
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
